bc04fbbc0e89fb328e29a9c8cb4c98a50495c302
luojiyin <luojiyin@hotmail.com>
2025-02-17 23:44:30 +0800

    finish algorithm10

--- exercises/easy/algorithm10.rs
+++ exercises/easy/algorithm10.rs
@@ -1,8 +1,7 @@
 /*
-	graph
-	This problem requires you to implement a basic graph functio
+    graph
+    This problem requires you to implement a basic graph function
 */
-
 
 use std::collections::{HashMap, HashSet};
 use std::fmt;
@@ -30,6 +29,20 @@ impl Graph for UndirectedGraph {
     }
     fn add_edge(&mut self, edge: (&str, &str, i32)) {
         //TODO
+        let (node1, node2, weight) = edge;
+        self.add_node(node1);
+        self.add_node(node2);
+
+        let adj_table = self.adjacency_table_mutable();
+
+        adj_table
+            .get_mut(node1)
+            .unwrap()
+            .push((node2.to_string(), weight));
+        adj_table
+            .get_mut(node2)
+            .unwrap()
+            .push((node1.to_string(), weight));
     }
 }
 pub trait Graph {
@@ -38,10 +51,34 @@ pub trait Graph {
     fn adjacency_table(&self) -> &HashMap<String, Vec<(String, i32)>>;
     fn add_node(&mut self, node: &str) -> bool {
         //TODO
-		true
+        if !self.contains(node) {
+            self.adjacency_table_mutable()
+                .insert(node.to_string(), Vec::new());
+            return true;
+        } else {
+            return false;
+        }
     }
     fn add_edge(&mut self, edge: (&str, &str, i32)) {
         //TODO
+        let (node1, node2, weight) = edge;
+
+        self.add_node(node1);
+        self.add_node(node2);
+
+        // 获取可变的邻接表引用
+        let adj_table = self.adjacency_table_mutable();
+
+        // 在node1的邻接表中添加node2和权重
+        adj_table
+            .get_mut(node1)
+            .unwrap()
+            .push((node2.to_string(), weight));
+        // 在node2的邻接表中添加node1和权重
+        adj_table
+            .get_mut(node2)
+            .unwrap()
+            .push((node1.to_string(), weight));
     }
     fn contains(&self, node: &str) -> bool {
         self.adjacency_table().get(node).is_some()
