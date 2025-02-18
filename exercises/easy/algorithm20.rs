ddf936587bba646e9669c1e94fdab5b68ae7b535
luojiyin <luojiyin@hotmail.com>
2025-02-18 18:40:52 +0800

    finish algorithm20

--- exercises/easy/algorithm20.rs
+++ exercises/easy/algorithm20.rs
@@ -7,11 +7,18 @@
     Hint: You can solve this problem using bitwise operations.
 */
 
-use std::fmt::{self, Display, Formatter};
+
 
 pub fn get_sum(a: i32, b: i32) -> i32 {
     // TODO: Implement the logic to calculate the sum of two integers without using `+`
-    0 // Placeholder return value
+    let mut a = a;
+    let mut b = b;
+    while b!=0  {
+        let carry = a &b;
+        a = a ^b;
+        b = carry <<1;
+    }
+    return a 
 }
 
 #[cfg(test)]
