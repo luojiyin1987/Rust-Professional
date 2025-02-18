d980846c8d9c633c832121bbda0f5651658c4314
luojiyin <luojiyin@hotmail.com>
2025-02-18 16:45:56 +0800

    finish algorithm16

--- exercises/easy/algorithm16.rs
+++ exercises/easy/algorithm16.rs
@@ -1,6 +1,6 @@
 /*
     Rotate Matrix 90 Degrees
-    Given a 2D matrix, rotate it 90 degrees in place. 
+    Given a 2D matrix, rotate it 90 degrees in place.
     You need to perform the rotation without using any additional matrix storage.
 
     You need to implement the function `rotate_matrix_90_degrees(matrix: &mut Vec<Vec<i32>>)`.
@@ -9,10 +9,24 @@
     Hint: Consider rotating the matrix layer by layer, starting from the outermost layer and working your way inward.
 */
 
-use std::fmt::{self, Display, Formatter};
-
 pub fn rotate_matrix_90_degrees(matrix: &mut Vec<Vec<i32>>) {
     // TODO: Implement the logic to rotate the matrix 90 degrees in place
+    let n = matrix.len();
+    if n == 0 {
+        return;
+    }
+
+    // 先转置矩阵
+    let n = matrix.len();
+    let m = matrix[0].len();
+    let mut new_matrix = vec![vec![0;n]; m];
+
+    for i in 0..n {
+        for j in 0..m {
+            new_matrix[j][n-i-1] = matrix[i][j];
+        }
+    }
+    *matrix = new_matrix;
 }
 
 #[cfg(test)]
@@ -21,58 +35,33 @@ mod tests {
 
     #[test]
     fn test_rotate_matrix_1() {
-        let mut matrix = vec![
-            vec![1, 2, 3],
-            vec![4, 5, 6],
-            vec![7, 8, 9],
-        ];
+        let mut matrix = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
         rotate_matrix_90_degrees(&mut matrix);
         println!("Rotated matrix: {:?}", matrix);
-        assert_eq!(matrix, vec![
-            vec![7, 4, 1],
-            vec![8, 5, 2],
-            vec![9, 6, 3],
-        ]);
+        assert_eq!(matrix, vec![vec![7, 4, 1], vec![8, 5, 2], vec![9, 6, 3],]);
     }
 
     #[test]
     fn test_rotate_matrix_2() {
-        let mut matrix = vec![
-            vec![1, 2],
-            vec![3, 4],
-        ];
+        let mut matrix = vec![vec![1, 2], vec![3, 4]];
         rotate_matrix_90_degrees(&mut matrix);
         println!("Rotated matrix: {:?}", matrix);
-        assert_eq!(matrix, vec![
-            vec![3, 1],
-            vec![4, 2],
-        ]);
+        assert_eq!(matrix, vec![vec![3, 1], vec![4, 2],]);
     }
 
     #[test]
     fn test_rotate_matrix_3() {
-        let mut matrix = vec![
-            vec![1],
-        ];
+        let mut matrix = vec![vec![1]];
         rotate_matrix_90_degrees(&mut matrix);
         println!("Rotated matrix: {:?}", matrix);
-        assert_eq!(matrix, vec![
-            vec![1],
-        ]);
+        assert_eq!(matrix, vec![vec![1],]);
     }
 
     #[test]
     fn test_rotate_matrix_4() {
-        let mut matrix = vec![
-            vec![1, 2],
-            vec![3, 4],
-            vec![5, 6],
-        ];
+        let mut matrix = vec![vec![1, 2], vec![3, 4], vec![5, 6]];
         rotate_matrix_90_degrees(&mut matrix);
         println!("Rotated matrix: {:?}", matrix);
-        assert_eq!(matrix, vec![
-            vec![5, 3, 1],
-            vec![6, 4, 2],
-        ]);
+        assert_eq!(matrix, vec![vec![5, 3, 1], vec![6, 4, 2],]);
     }
 }
