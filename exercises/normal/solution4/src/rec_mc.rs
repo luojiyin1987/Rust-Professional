5a1b3c11d9f3bc4dda5bfe5f4611e532ca7cba7d
luojiyin <luojiyin@hotmail.com>
2025-02-19 11:19:15 +0800

    finish normal4

--- exercises/normal/solution4/src/rec_mc.rs
+++ exercises/normal/solution4/src/rec_mc.rs
@@ -1,4 +1,24 @@
 pub fn dp_rec_mc(amount: u32) -> u32 {
-    // TODO: 这里写逻辑
-    todo!()
+   // 确保硬币面额按升序排列
+   let coins = [1, 2, 5, 10, 20, 50, 100];
+    
+   // 创建 DP 数组
+   let mut dp = vec![std::u32::MAX; (amount + 1) as usize];
+   
+   // 基本情况：0 金额需要 0 个硬币
+   dp[0] = 0;
+   
+   // 构建解决方案
+   for i in 1..=amount {
+       for &coin in coins.iter() {
+           if coin <= i {
+               let sub_res = dp[(i - coin) as usize];
+               if sub_res != std::u32::MAX {
+                   dp[i as usize] = dp[i as usize].min(sub_res + 1);
+               }
+           }
+       }
+   }
+   
+   dp[amount as usize]
 }
