2be5149e0ad0602c6030e8951b6c662aca634b57
luojiyin <luojiyin@hotmail.com>
2025-02-19 10:56:23 +0800

    finish normal3

--- exercises/normal/solution3/src/calc_logic.rs
+++ exercises/normal/solution3/src/calc_logic.rs
@@ -1,4 +1,17 @@
 pub fn new_birthday_probability(n: u32) -> f64 {
     // TODO: 这里写逻辑
-    todo!()
+    if n < 2 {
+        return 0.0;
+    }
+    if n > 365 {
+        return 1.0;
+    }
+
+    let mut probability = 1.0;
+
+    for i in 0..n {
+        probability *= (365 - i) as f64 / 365.0;
+    }
+
+    1.0 - probability
 }
