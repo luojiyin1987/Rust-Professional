18c4ebc678538e9611255e76f9b2abcd33361491
luojiyin <luojiyin@hotmail.com>
2025-02-19 08:37:09 +0800

    finish normal1

--- exercises/normal/solution1/src/count_distinct.rs
+++ exercises/normal/solution1/src/count_distinct.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
 pub fn new_count_distinct(input_str: &str) -> usize {
-    todo!()
+    let unique_items: HashSet<&str> = input_str.split(',').collect();
+    unique_items.len()
 }
